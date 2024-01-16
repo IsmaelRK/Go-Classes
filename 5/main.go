a2897d77d9d409034550c5fc8d769d58b41b3071
IsmaelRK <ismael2021ab@gmail.com>
2024-01-16 18:15:05 -0300

    add

--- 5/main.go
+++ 5/main.go
@@ -1,14 +1,29 @@
 package main
 
+import "fmt"
+
 type people struct {
 	nome     string
 	lastName string
 	age      uint8
-	height   uint8
+	height   float32
 }
 
 type student struct {
 	people
 	course     string
 	university string
+}
+
+func main() {
+
+	fmt.Println(student{})
+
+	p := people{"Ismael", "R", 16, 1.63}
+	s := student{p, "IT", "UFSM"}
+
+	fmt.Println(s)
+	fmt.Println(s.course)
+	fmt.Println(s.people.nome)
+
 }
