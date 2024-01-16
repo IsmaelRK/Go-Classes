e4a5a18529cdb6e9c2ceb54b31bc9b444e17b115
IsmaelRK <ismael2021ab@gmail.com>
2024-01-16 20:01:26 -0300

    add

--- 9/main.go
+++ 9/main.go
@@ -15,4 +15,21 @@ func main() {
 	if otherNum := num + 1; otherNum > 5 {
 		fmt.Println("Yep", otherNum)
 	}
+
+	// switch
+
+	switch num {
+
+	case 5:
+		println("Vale 5")
+
+	case 6:
+		println("Vale 6")
+		return
+
+	default:
+		println("Default")
+
+	}
+
 }
