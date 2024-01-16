e58a798fb0d1899bb57f3184333b296fbefd3bf2
IsmaelRK <ismael2021ab@gmail.com>
2024-01-16 16:48:31 -0300

    add

---
+++ 3/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func sum(num int8, num2 int8) int8 {
+	r := num + num2
+	return r
+}
+
+func main() {
+
+	var (
+		n1 int8 = 2
+		n2 int8 = 3
+	)
+
+	val := sum(n1, n2)
+
+	fmt.Println(val)
+}
