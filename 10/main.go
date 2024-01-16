f6380beba73ce77c1311203c4238b3715910ed99
IsmaelRK <ismael2021ab@gmail.com>
2024-01-16 20:15:36 -0300

    add

---
+++ 10/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//i := 0
+
+	//for i < 10 {
+	//	i++
+	//	fmt.Println("plus", i)
+	//	time.Sleep(time.Second)
+	//}
+	//fmt.Println(i)
+	//
+	//for j := 0; j < 10; j++ {
+	//	fmt.Println("plus", j)
+	//}
+
+	names := [3]string{"Ismael", "Joao", "Pedro"}
+
+	for index, name := range names {
+		fmt.Println(index, name)
+	}
+
+}
