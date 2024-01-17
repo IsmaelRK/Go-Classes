e7280ec09a558b146d2e1ec621f9ead3ade5d5cb
IsmaelRK <ismael2021ab@gmail.com>
2024-01-17 14:00:04 -0300

    added

--- 21 - CLI/app/app.go
+++ 21 - CLI/app/app.go
@@ -12,18 +12,26 @@ func Generate() *cli.App {
 	app.Name = "Cli Application"
 	app.Usage = "Search for ips and server names on internet"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "devbook.com.br",
+		},
+	}
+
 	app.Commands = []cli.Command{
+		{
+			Name:   "ip",
+			Usage:  "Search for ips",
+			Flags:  flags,
+			Action: searchIps,
+		},
 
 		{
-			Name:  "ip",
-			Usage: "Search for ips",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "devbook.com.br",
-				},
-			},
-			Action: searchIps,
+			Name:   "servers",
+			Usage:  "Search for servers name",
+			Flags:  flags,
+			Action: searchServers,
 		},
 	}
 
@@ -42,6 +50,21 @@ func searchIps(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+
+}
+
+func searchServers(c *cli.Context) {
+
+	host := c.String("host")
+	servers, erro := net.LookupNS(host)
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, server := range servers {
+		fmt.Println(server.Host)
 	}
 
 }
