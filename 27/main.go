4ccaab3076793ce6ba8ef01a4090625dbbf45174
IsmaelRK <ismael2021ab@gmail.com>
2024-01-17 20:02:07 -0300

    added

--- 27/main.go
+++ 27/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func addrstype(adress string) string {
+
+	validTypes := []string{"rua", "avenida", "estrada", "rodovia"}
+
+	adress = strings.ToLower(adress)
+	firtWord := strings.Split(adress, " ")[0]
+
+	var resultType string
+
+	for _, validType := range validTypes {
+
+		if firtWord == validType {
+			resultType = validType
+			break
+		} else {
+			resultType = "Not Found"
+		}
+
+	}
+
+	return resultType
+}
+
+func main() {
+	fmt.Println(addrstype("rua 1"))
 }
