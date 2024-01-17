0d2259d2ef6c0c4981e9e3e32c1fa20bc3e5526b
IsmaelRK <ismael2021ab@gmail.com>
2024-01-16 23:28:18 -0300

    add

--- 14/main.go
+++ 14/main.go
@@ -1,5 +1,33 @@
 package main
 
+import "fmt"
+
+func recoverApp() {
+	fmt.Println("Recovering")
+
+	if r := recover(); r != nil { // if there isnt anything to recover r == nil
+		fmt.Println("Recovered")
+	}
+
+}
+func grade(num int) {
+	defer recoverApp()
+	if num > 5 {
+		fmt.Println("Ok!")
+		return
+	} else if num < 5 {
+		fmt.Println("Ok")
+		return
+	}
+
+	panic("NUM IS 5!")
+
+}
+
 func main() {
 
+	num := 5
+	grade(num)
+
+	fmt.Println("Finished")
 }
