2cd5db5a666f19722884ef2abe0b81afde8b8728
IsmaelRK <ismael2021ab@gmail.com>
2024-01-17 16:45:45 -0300

    added

--- 24/main.go
+++ 24/main.go
@@ -1,1 +1,60 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+//func main() {
+//
+//	chanel := make(chan string)
+//	go write("Hello World", chanel)
+//
+//	msg := <-chanel
+//	fmt.Println(msg)
+//
+//}
+//
+//func write(txt string, chanel chan string) {
+//
+//	for i := 0; i < 5; i++ {
+//
+//		chanel <- txt
+//		time.Sleep(time.Second)
+//	}
+//
+//}
+
+func main() {
+
+	chanel := make(chan string)
+	go write("Hello World", chanel)
+
+	//for {
+	//
+	//	msg, opened := <-chanel
+	//	fmt.Println(msg)
+	//
+	//	if !opened {
+	//		break
+	//	}
+	//
+	//}
+
+	for msg := range chanel {
+		fmt.Println(msg)
+	}
+
+}
+
+func write(txt string, chanel chan string) {
+
+	for i := 0; i < 5; i++ {
+
+		chanel <- txt
+		time.Sleep(time.Second)
+	}
+
+	close(chanel)
+
+}
