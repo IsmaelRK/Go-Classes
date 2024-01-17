d0c888eefcd9d28d45a0842516d2ae0e48a0dfc0
IsmaelRK <ismael2021ab@gmail.com>
2024-01-17 14:26:02 -0300

    added

--- 22/main.go
+++ 22/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	go prt("GoRoutine")
+	prt("Regular Function")
+
+}
+
+func prt(str string) {
+
+	for {
+		fmt.Println(str)
+		time.Sleep(time.Second)
+	}
+
+}
