66ed181b2180c03288d78adf2161b0d0e25c5e17
IsmaelRK <ismael2021ab@gmail.com>
2024-01-17 21:38:16 -0300

    added

--- 27/main_test.go
+++ 27/main_test.go
@@ -5,17 +5,46 @@ import (
 	"testing"
 )
 
+type test struct {
+	insert      string
+	returnValue string
+}
+
 func TestAddrsType(t *testing.T) {
 
-	tstAddres := "Avenida XYZ"
-	addrWaited := "avenida"
+	mytests := []test{
+		{"Rua 1", "rua"},
+		{"Avenida 1", "avenida"},
+		{"Logradouro 378", "not found"},
+	}
 
-	addressTypeReceived := Addrstype(tstAddres)
+	for _, key := range mytests {
 
-	fmt.Println(addrWaited, addressTypeReceived)
+		fmt.Println(key)
 
-	if addrWaited != addressTypeReceived {
-		t.Errorf("The type is different!")
+		addressTypeReceived := Addrstype(key.insert)
+
+		fmt.Println(key.insert, addressTypeReceived)
+
+		if addressTypeReceived != key.returnValue {
+			t.Errorf("The type is different!")
+		}
+
 	}
 
 }
+
+//func TestAddrsType(t *testing.T) {
+//
+//	tstAddres := "Avenida XYZ"
+//	addrWaited := "avenida"
+//
+//	addressTypeReceived := Addrstype(tstAddres)
+//
+//	fmt.Println(addrWaited, addressTypeReceived)
+//
+//	if addrWaited != addressTypeReceived {
+//		t.Errorf("The type is different!")
+//	}
+//
+//}
