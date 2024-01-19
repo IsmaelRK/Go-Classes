d8b98b1c154673a6b3c06875339f4d61def04607
IsmaelRK <ismael2021ab@gmail.com>
2024-01-19 14:46:40 -0300

    added

--- 31/main.go
+++ 31/main.go
@@ -1,7 +1,37 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
 func main() {
 
-	urlconn := "ismael:4682"
+	urlconn := "ismael:adm123@/golang?charset=utf8&parseTime=True&loc=Local"
+	db, erro := sql.Open("mysql", urlconn)
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	defer db.Close()
+
+	if erro = db.Ping(); erro != nil {
+		log.Fatal(erro)
+	}
+
+	fmt.Println("Opened Conn")
+
+	lines, erro := db.Query("select * from users")
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	defer lines.Close()
+
+	fmt.Println(lines)
 
 }
