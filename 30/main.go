a26dd7a44eb84ee73ef7e5b78d5b78ee22c21ef5
IsmaelRK <ismael2021ab@gmail.com>
2024-01-19 11:34:19 -0300

    added

--- 30/main.go
+++ 30/main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
+var templates *template.Template
+
 func home(w http.ResponseWriter, r *http.Request) {
 
-	w.Write([]byte("<h1>Hello World</h1>"))
+	//w.Write([]byte("<h1>Hello World</h1>"))
+	templates.ExecuteTemplate(w, "index.html", nil)
 
 }
 
 func main() {
 
+	templates = template.Must(template.ParseGlob("*html"))
+
 	http.HandleFunc("/home", home)
 
+	fmt.Println("Listening on :5000")
 	log.Fatal(http.ListenAndServe(":5000", nil))
 
 }
