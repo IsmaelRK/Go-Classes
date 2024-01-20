867020a3ea8132064bcc1ae3a205f70a6b934e28
IsmaelRK <ismael2021ab@gmail.com>
2024-01-20 16:25:38 -0300

    added

--- 32/server/server.go
+++ 32/server/server.go
@@ -1,8 +1,8 @@
 package server
 
 import (
+	"crud/db"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 )
@@ -28,6 +28,26 @@ func CreateUsr(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Erro ao converter"))
 	}
 
-	fmt.Println(user)
+	dbconn, erro := db.Connect()
+	if erro != nil {
+		w.Write([]byte("Erro ao conectar"))
+		return
+	}
+	defer dbconn.Close()
+
+	statement, erro := dbconn.Prepare("insert into users (nome, email) values (?, ?)")
+	if erro != nil {
+		w.Write([]byte("Erro ao criar statement"))
+		return
+	}
+	defer statement.Close()
+
+	insertion, err := statement.Exec(user.Name, user.Email)
+	if err != nil {
+		w.Write([]byte("Erro ao executar statement"))
+		return
+	}
+
+	idInserted := insertion.LastInsertId()
 
 }
