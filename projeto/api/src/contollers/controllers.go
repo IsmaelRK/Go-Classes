de4a694c6dcf9628c01f7e050b99d0cee1d974d8
IsmaelRK <ismael2021ab@gmail.com>
2024-01-21 20:09:58 -0300

    added

--- projeto/api/src/contollers/controllers.go
+++ projeto/api/src/contollers/controllers.go
@@ -4,20 +4,30 @@ import "net/http"
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
 
+	w.Write([]byte("Creating User"))
+
 }
 
 func SearchUsers(w http.ResponseWriter, r *http.Request) {
+
+	w.Write([]byte("Searching Users"))
 
 }
 
 func SearchUser(w http.ResponseWriter, r *http.Request) {
 
+	w.Write([]byte("Searching User"))
+
 }
 
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
 
+	w.Write([]byte("Updating User"))
+
 }
 
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
+
+	w.Write([]byte("Deleting User"))
 
 }
