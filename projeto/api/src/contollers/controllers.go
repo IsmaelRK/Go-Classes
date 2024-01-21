00c1b07dab3b882caf1497ba1b4063d260403afc
IsmaelRK <ismael2021ab@gmail.com>
2024-01-21 16:43:13 -0300

    added

---
+++ projeto/api/src/contollers/controllers.go
@@ -0,0 +1,23 @@
+package contollers
+
+import "net/http"
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func SearchUsers(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func SearchUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+
+}
--- projeto/api/src/router/routes/userRoutes.go
+++ projeto/api/src/router/routes/userRoutes.go
@@ -1,55 +1,43 @@
 package routes
 
-import "net/http"
+import (
+	"api/src/contollers"
+	"net/http"
+)
 
 var routes = []Route{
 	{
-		Uri:    "/users",
-		Method: http.MethodPost,
-		Function: func(w http.ResponseWriter, r *http.Request) {
-
-		},
-
+		Uri:         "/users",
+		Method:      http.MethodPost,
+		Function:    contollers.CreateUser,
 		AuthRequire: false,
 	},
 
 	{
-		Uri:    "/users",
-		Method: http.MethodGet,
-		Function: func(w http.ResponseWriter, r *http.Request) {
-
-		},
-
+		Uri:         "/users",
+		Method:      http.MethodGet,
+		Function:    contollers.SearchUsers,
 		AuthRequire: false,
 	},
 
 	{
-		Uri:    "/users/{userId}",
-		Method: http.MethodGet,
-		Function: func(w http.ResponseWriter, r *http.Request) {
-
-		},
-
+		Uri:         "/users/{userId}",
+		Method:      http.MethodGet,
+		Function:    contollers.SearchUser,
 		AuthRequire: false,
 	},
 
 	{
-		Uri:    "/users/{userId}",
-		Method: http.MethodPut,
-		Function: func(w http.ResponseWriter, r *http.Request) {
-
-		},
-
+		Uri:         "/users/{userId}",
+		Method:      http.MethodPut,
+		Function:    contollers.UpdateUser,
 		AuthRequire: false,
 	},
 
 	{
-		Uri:    "/users/{userId}",
-		Method: http.MethodDelete,
-		Function: func(w http.ResponseWriter, r *http.Request) {
-
-		},
-
+		Uri:         "/users/{userId}",
+		Method:      http.MethodDelete,
+		Function:    contollers.DeleteUser,
 		AuthRequire: false,
 	},
 }
