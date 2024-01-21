8abd83216fbc12571f52a4f187b0674e880411c5
IsmaelRK <ismael2021ab@gmail.com>
2024-01-21 16:57:54 -0300

    added

--- projeto/api/src/router/routes/routes.go
+++ projeto/api/src/router/routes/routes.go
@@ -1,10 +1,26 @@
 package routes
 
-import "net/http"
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
 
 type Route struct {
 	Uri         string
 	Method      string
 	Function    func(http.ResponseWriter, *http.Request)
 	AuthRequire bool
+}
+
+func ConfigRoutes(r *mux.Router) *mux.Router {
+
+	routes := usrRoutes
+
+	for _, route := range routes {
+
+		r.HandleFunc(route.Uri, route.Function).Methods(route.Method)
+
+	}
+
+	return r
 }
--- projeto/api/src/router/routes/userRoutes.go
+++ projeto/api/src/router/routes/userRoutes.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-var routes = []Route{
+var usrRoutes = []Route{
 	{
 		Uri:         "/users",
 		Method:      http.MethodPost,
