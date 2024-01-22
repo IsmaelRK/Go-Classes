a7dbb9230cb79c9a5a41fe6efb5fb2241ff82bc2
IsmaelRK <ismael2021ab@gmail.com>
2024-01-22 08:48:34 -0300

    added

--- projeto/api/src/database/database.go
+++ projeto/api/src/database/database.go
@@ -1,7 +1,22 @@
 package database
 
-import "database/sql"
+import (
+	"api/src/config"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func Conn() (*sql.DB, error) {
 
+	db, err := sql.Open("mysql", config.DbConn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
