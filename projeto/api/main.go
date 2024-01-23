dd67d946a9c1ae72e6e1cb60836d1105145a7e69
IsmaelRK <ismael2021ab@gmail.com>
2024-01-23 13:23:18 -0300

    added

--- projeto/api/main.go
+++ projeto/api/main.go
@@ -3,10 +3,25 @@ package main
 import (
 	"api/src/config"
 	"api/src/router"
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+func init() {
+
+	key := make([]byte, 64)
+
+	if _, err := rand.Read(key); err != nil {
+		log.Fatal(err)
+	}
+
+	string64 := base64.StdEncoding.EncodeToString(key)
+	fmt.Println(string64)
+
+}
 
 func main() {
 
